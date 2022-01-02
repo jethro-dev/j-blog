b218229545b4f58e027f1543d39a21ad372e75ad
Jethro Au <supergalong@gmail.com>
2022-01-02 03:24:07 +0000

    bug fix

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -12,26 +12,32 @@ import {
 import { useRouter } from "next/router";
 
 const Post = ({ post }) => {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <>
       <Head>
-        <title>{post.title}</title>
+        <title>{post?.title}</title>
       </Head>
       <section className="bg-zinc-200 dark:bg-gray-500 transition-colors px-5">
         <div className="max-w-7xl mx-auto py-10">
           <div className="grid grid-cols-1 md:grid-cols-12 md:gap-6">
             <div className="md:col-span-8 col-span-1 ">
               <PostDetail post={post} />
-              <CommentsForm slug={post.slug} />
-              <Comments slug={post.slug} />
+              <CommentsForm slug={post?.slug} />
+              <Comments slug={post?.slug} />
             </div>
 
             <div className="md:col-span-4 col-span-1">
               <div className="md:sticky relative md:top-[110px]">
-                <Author author={post.author} />
+                <Author author={post?.author} />
                 <PostWidget
-                  slug={post.slug}
-                  categories={post.categories.map((category) => category.slug)}
+                  slug={post?.slug}
+                  categories={post?.categories.map((category) => category.slug)}
                 />
                 <Categories />
               </div>
